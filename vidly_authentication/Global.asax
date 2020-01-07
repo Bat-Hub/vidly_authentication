0e1b12fcf32eae540bffd20f5e3d280d754ebf52
Bat-Hub <pras.ar90@gmail.comm>
2020-01-07 07:28:48 +0530

    Add project files.

---
+++ vidly_authentication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vidly_authentication.MvcApplication" Language="C#" %>
---
+++ vidly_authentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly_authentication.Startup))]
+namespace vidly_authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
